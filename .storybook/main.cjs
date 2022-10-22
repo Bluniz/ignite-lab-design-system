14919d24141f8e0c8bd5a7e8e91ffa495e732098
Lucas Rosa <lucassantosrosa51@gmail.com>
2022-10-21 22:21:26 -0300

    chore: Add Pulish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,18 +1,23 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
   },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+  features: {
+    storyStoreV7: true,
+  },
+  //! CONFIGURAÇÃO PARA GARANTIR QUE O STORYBOOK NÃO SE PERCA ACHANDO QUE TA ABRINDO PASTAS.
+  viteFinal: (config, { configType }) => {
+    if (configType === "production") {
+      config.base = "/ignite-lab-design-system/";
+    }
+
+    return config;
+  },
+};
